1e36dc85e46d93c13ecf43026e82541d0046ee77
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-10-25 11:26:08 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Impact", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(399, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nová hra";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Impact", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(12, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(399, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Načíst hru";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Orange;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Impact", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(12, 444);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(399, 64);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Nastavení";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Orange;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Impact", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(12, 514);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(399, 64);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ukončit hru";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(131, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 68);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Menu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(423, 592);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form novahra = new Form();
+        Form nastaveni = new Form();
+        Form nacisthru = new Form();
+        Form ukoncithru = new Form();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBox jmeno = new TextBox();
+            jmeno.Location = new Point(50, 0);
+            Button ulozit = new Button();
+            ulozit.Size = new Size(100, 50);
+            ulozit.Location = new Point(50, 30);
+            ulozit.Text = "Uložit";
+            ulozit.DialogResult = DialogResult.OK;
+            novahra.Controls.Add(jmeno);
+            novahra.Controls.Add(ulozit);
+            novahra.ShowDialog();
+            if (ulozit.DialogResult == DialogResult.OK)
+            {
+                FileStream textak = new FileStream("hra.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter zapisovac = new StreamWriter(textak);
+                string jmenoxd = jmeno.Text;
+                zapisovac.Write("Jméno: " + jmenoxd + ", datum a čas vytvoření: " + DateTime.Now);
+                zapisovac.Close();
+                MessageBox.Show("Jméno bylo uloženo :D");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CheckBox fullscreen = new CheckBox();
+            fullscreen.Location = new Point(10, 100);
+            fullscreen.Text = "Režim celé obrazovky";
+            RadioButton cervena = new RadioButton();
+            cervena.Location = new Point(10, 10);
+            cervena.Text = "Červená";
+            RadioButton zelena = new RadioButton();
+            zelena.Location = new Point(10, 40);
+            zelena.Text = "Zelená";
+            RadioButton modra = new RadioButton();
+            modra.Location = new Point(10, 70);
+            modra.Text = "Modrá";
+            Button ulozit = new Button();
+            ulozit.Size = new Size(100, 50);
+            ulozit.Location = new Point(50, 200);
+            ulozit.Text = "Uložit nastavení";
+            ulozit.DialogResult = DialogResult.OK;
+            Button zpet = new Button();
+            zpet.Size = new Size(100, 50);
+            zpet.Location = new Point(152, 200);
+            zpet.Text = "Zpět do menu";
+            zpet.DialogResult = DialogResult.Cancel;
+
+            nastaveni.Controls.Add(zpet);
+            nastaveni.Controls.Add(ulozit);
+            nastaveni.Controls.Add(cervena);
+            nastaveni.Controls.Add(zelena);
+            nastaveni.Controls.Add(modra);
+            nastaveni.Controls.Add(fullscreen);
+
+            nastaveni.ShowDialog();
+            if (ulozit.DialogResult == DialogResult.OK)
+            {
+                if(cervena.Checked == true)
+                {
+                    BackColor = Color.Red;
+                    novahra.BackColor = Color.Red;
+                    nastaveni.BackColor = Color.Red;
+                    nacisthru.BackColor = Color.Red;
+                    ukoncithru.BackColor = Color.Red;
+
+                }
+                else if(zelena.Checked == true)
+                {
+                    BackColor = Color.Green;
+                    novahra.BackColor = Color.Green;
+                    nastaveni.BackColor = Color.Green;
+                    nacisthru.BackColor = Color.Green;
+                    ukoncithru.BackColor = Color.Green;
+                }
+                else if (modra.Checked == true)
+                {
+                    BackColor = Color.Blue;
+                    novahra.BackColor = Color.Blue;
+                    nastaveni.BackColor = Color.Blue;
+                    nacisthru.BackColor = Color.Blue;
+                    ukoncithru.BackColor = Color.Blue;
+                }
+                if(fullscreen.Checked == true)
+                {
+                    Size = new Size(1920, 1080);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream textak = new FileStream("hra.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader ctenar = new StreamReader(textak);
+            if (!ctenar.EndOfStream)
+            {                          
+                Label label = new Label();
+                label.Location = new Point(50, 50);
+                label.Size = new Size(500, 20);
+                label.Text = "Načtená hra: " + ctenar.ReadLine();
+                nacisthru.Controls.Add(label);
+            }
+            ctenar.Close();
+            
+            nacisthru.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Label label = new Label();
+            label.Location = new Point(50, 50);
+            label.Size = new Size(200, 20);
+            label.Text = "Opravdu chcete ukončit hru?";
+            Button ano = new Button();
+            ano.Size = new Size(100, 50);
+            ano.Location = new Point(50, 200);
+            ano.Text = "Ano";
+            ano.DialogResult = DialogResult.OK;
+            Button ne = new Button();
+            ne.Size = new Size(100, 50);
+            ne.Location = new Point(152, 200);
+            ne.Text = "Ne";
+            ne.DialogResult = DialogResult.Cancel;
+            ukoncithru.Controls.Add(ano);
+            ukoncithru.Controls.Add(ne);
+            ukoncithru.Controls.Add(label);
+            ukoncithru.ShowDialog();
+            if (ano.DialogResult == DialogResult.OK)
+            {
+                 this.Close();
+            }
+        }
+    }
+}
